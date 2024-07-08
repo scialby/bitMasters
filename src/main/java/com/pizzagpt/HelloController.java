765f6e18c9fddc689b2bf8435784d35692663c3c
unknown <sortinodavide9@gmail.com>
2024-07-08 15:34:46 +0200

    fixes

--- src/main/java/com/pizzagpt/HelloController.java
+++ src/main/java/com/pizzagpt/HelloController.java
@@ -1,14 +1,70 @@
 package com.pizzagpt;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Button button;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label wrongLogIn;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
+
+    public void readFile(String filename){
+        try {
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+    }
+    public void userLogin(ActionEvent event) throws IOException {
+        checkLogin();
+
+    }
+
+    public void userRegister(ActionEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("test.fxml");
+
+    }
+
+    private void checkLogin() throws IOException {
+
+        //get data from users.txt
+        //check if username and password match
+        //if match, change scene
+        //if not, display error message
+        // file name is users.txt
+        // format is username,password
+
+        File file = new File("users.txt");
+        if(file.exists()) {
+
+        } else {
+            wrongLogIn.setText("No users found");
+        }
     }
 }
--- src/main/java/com/pizzagpt/Main.java
+++ src/main/java/com/pizzagpt/Main.java
@@ -8,15 +8,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+    private static Stage stg;
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
+        stg = primaryStage;
+
         FXMLLoader fxmlLoader = new FXMLLoader(com.pizzagpt.Main.class.getResource("/scenes/MainScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
+    public void changeScene(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(com.pizzagpt.Main.class.getResource("/scenes/" + fxml));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stg.setScene(scene);
+        stg.show();
+    }
     public static void main(String[] args) {
         launch();
     }
