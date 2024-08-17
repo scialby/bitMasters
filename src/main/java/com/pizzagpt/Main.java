4ed8d0ed825760d39006fd073f354c555c2a0c53
scialby <sortinodavide9@gmail.com>
2024-08-17 15:29:49 +0200

    Riorganizzazione packages e file per avere una struttura del progetto piu ordinata, sarà da finire il login system

--- src/main/java/com/pizzagpt/Main.java
+++ src/main/java/com/pizzagpt/Main.java
@@ -9,19 +9,18 @@ import java.io.IOException;
 
 public class Main extends Application {
     private static Stage stg;
-
     @Override
     public void start(Stage primaryStage) throws IOException {
         stg = primaryStage;
 
-        FXMLLoader fxmlLoader = new FXMLLoader(com.pizzagpt.Main.class.getResource("/scenes/MainScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com.pizzagpt.scenes.userSession/LoginScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         primaryStage.setTitle("Hello!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    public void changeScene(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(com.pizzagpt.Main.class.getResource("/scenes/" + fxml));
+    public static void changeScene(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com.pizzagpt.scenes.userSession/" + fxml));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stg.setScene(scene);
         stg.show();
